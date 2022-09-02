4292b2deac7a634f8bbe950cb05b65d26dd61376
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-09-02 23:39:55 +0100

    ready for deployment

--- src/pages/Abouts/Header/Header.tsx
+++ src/pages/Abouts/Header/Header.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi"
+import { AiOutlineClose } from "react-icons/ai"
+
 import logo from "../../../assets/Logo Shape.png";
 import { navItems } from "../../../helpers/utils";
 
@@ -17,16 +19,19 @@ export default function Header() {
           </h1>
         </div>
         <div className="hamburger sm:hidden block">
-          <GiHamburgerMenu className="text-white text-3xl" onClick={() => setNavOpen(!navOpen)} />
+          <GiHamburgerMenu className="text-white text-3xl z-[999]" onClick={() => setNavOpen(!navOpen)} />
         </div>
         {/* desktop and mobile */}
-        <ul className={`sm:flex items-center hidden ${navOpen ? "flex" :""}`}>
+        <ul className={`flex items-center  ${navOpen ? "fixed top-0 left-0 right-0 bottom-0 flex-col bg-[#0A2640] w-full h-[100%] justify-center duration-200" : "hidden"}`}>
+          <div className="hamburger sm:hidden block absolute top-0 right-0 m-10">
+            <AiOutlineClose className="text-white text-5xl z-[999]" onClick={() => setNavOpen(!navOpen)} />
+          </div>
           {navItems.map((item) => (
-            <li className="mx-5 text-[#FFFFFF] font-[600] text-[16px]">
+            <li className="sm:mx-5 my-5 text-[#FFFFFF] font-[600] sm:text-[16px] text-[25px]">
               <Link to={item.path}>{item.item}</Link>
             </li>
           ))}
-          <li className="bg-[#FFFFFF] text-[#0A2640] rounded-[24px] py-3 px-8 cursor-pointer">
+          <li className="bg-[#FFFFFF] text-[#0A2640] sm:rounded-[24px] py-3 px-8 cursor-pointer sm:my-0 my-5">
             Log In
           </li>
         </ul>
